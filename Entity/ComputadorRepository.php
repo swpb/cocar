c94cf06e8b4555e649001725dfc2a7ff588cd041
Eduardo Santos <eduardo@eduardosan.com>
2015-12-04 11:21:11 -0200

    Mostra monitoramento do Cocar na tela inicial

--- Entity/ComputadorRepository.php
+++ Entity/ComputadorRepository.php
@@ -12,4 +12,46 @@ use Doctrine\ORM\EntityRepository;
  */
 class ComputadorRepository extends EntityRepository
 {
+    /**
+     * Máquinas que não estão no Cacic
+     *
+     * @return mixed
+     */
+    public function cacicNull() {
+        $start = isset($start) ? $start : (time() - ((60*60*24)*30));
+        $start = new \DateTime(date("Y-m-d", $start));
+        $end   = isset($end) ? $end : time();
+        $end = new \DateTime(date("Y-m-d", $end));
+
+        $qb = $this->createQueryBuilder('c')
+            ->select("count(DISTINCT c.id)")
+            ->innerJoin("CocarBundle:PingComputador", "p")
+            ->andWhere("c.cacic_id is null")
+            ->andWhere("p.date BETWEEN :start AND :end")
+            ->setParameter("start", $start)
+            ->setParameter("end", $end);
+
+        return $qb->getQuery()->getSingleScalarResult();
+    }
+
+    /**
+     * Total de computadores que pingaram nos últimos 30 dias
+     *
+     * @return mixed
+     */
+    public function totalComp() {
+        $start = isset($start) ? $start : (time() - ((60*60*24)*30));
+        $start = new \DateTime(date("Y-m-d", $start));
+        $end   = isset($end) ? $end : time();
+        $end = new \DateTime(date("Y-m-d", $end));
+
+        $qb = $this->createQueryBuilder('c')
+            ->select("count(DISTINCT c.id)")
+            ->innerJoin("CocarBundle:PingComputador", "p")
+            ->andWhere("p.date BETWEEN :start AND :end")
+            ->setParameter("start", $start)
+            ->setParameter("end", $end);
+
+        return $qb->getQuery()->getSingleScalarResult();
+    }
 }
