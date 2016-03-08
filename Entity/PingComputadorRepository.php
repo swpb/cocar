8a7c56b54f6fd4be97cbf14373e7a50a2f229a50
Eduardo Santos <eduardo@eduardosan.com>
2016-03-08 00:30:59 -0300

    Peequena correção no relatório de computadores do Cocar.

--- Entity/PingComputadorRepository.php
+++ Entity/PingComputadorRepository.php
@@ -20,6 +20,11 @@ class PingComputadorRepository extends EntityRepository
      * @return mixed
      */
     public function relatorioGeral($start, $end) {
+        $start = isset($start) ? $start : (time() - ((60*60*24)*30));
+        $start = new \DateTime(date("Y-m-d", $start));
+        $end   = isset($end) ? $end : time();
+        $end = new \DateTime(date("Y-m-d", $end));
+
         $qb = $this->createQueryBuilder('p')
             ->select(
                 'max(p.date) as last_ping',
